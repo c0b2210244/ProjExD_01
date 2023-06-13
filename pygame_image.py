0ea59f1e268e9673b1f96b6ee3b77c2f7c3032c7
tsujiuta <c0b2210244@edu.teu.ac.jp>
2023-06-13 16:18:50 +0900

    演習問題　問題1

--- pygame_image.py
+++ pygame_image.py
@@ -8,25 +8,26 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     koukaton_img = pg.image.load("ex01/fig/3.png")
+    angle = 0
     koukaton_1 = pg.transform.flip(koukaton_img, True, False)
-    koukaton_2 = pg.transform.rotozoom(koukaton_1, 10, 1.0)
-    koukaton_list = [koukaton_1, koukaton_2]
     tmr = 0
     bg_x = 0
+    plus_angle = 0.1
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [-bg_x, 0])
-        if tmr % 2 == 0:
-            screen.blit(koukaton_list[0], [300, 200])
-        else:
-            screen.blit(koukaton_list[1], [300, 200])
+        koukaton_2 = pg.transform.rotozoom(koukaton_1, angle, 1.0)
+        screen.blit(koukaton_2, [300, 200])
         pg.display.update()
         tmr += 1
+        angle += plus_angle
+        if angle > 10 or angle <= 0:
+            plus_angle *= -1
         bg_x += 1
-        if bg_x > 1600:
-            bg_x = 0
+        if bg_x >= 1600:
+            bg_x = 1
         clock.tick(100)
 
 
