d9d00d243a89a56ea717d3c073e9b7e00f44a9e1
tsujiuta <c0b2210244@edu.teu.ac.jp>
2023-06-13 17:02:35 +0900

    演習問題　問題2

--- pygame_image.py
+++ pygame_image.py
@@ -7,6 +7,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img_rev = pg.transform.flip(bg_img, True, False)
     koukaton_img = pg.image.load("ex01/fig/3.png")
     angle = 0
     koukaton_1 = pg.transform.flip(koukaton_img, True, False)
@@ -16,8 +17,11 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
+        
         screen.blit(bg_img, [-bg_x, 0])
+        screen.blit(bg_img_rev, [1600 - bg_x, 0])
+        screen.blit(bg_img, [3200 - bg_x, 0])
+
         koukaton_2 = pg.transform.rotozoom(koukaton_1, angle, 1.0)
         screen.blit(koukaton_2, [300, 200])
         pg.display.update()
@@ -26,9 +30,9 @@ def main():
         if angle > 10 or angle <= 0:
             plus_angle *= -1
         bg_x += 1
-        if bg_x >= 1600:
-            bg_x = 1
-        clock.tick(100)
+        if bg_x >= 3200:
+            bg_x = 0
+        clock.tick(200)
 
 
 if __name__ == "__main__":
