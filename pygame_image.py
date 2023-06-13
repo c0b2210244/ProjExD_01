ae16d279f9755a733a12c7604dfde2e5deb5a411
tsujiuta <c0b2210244@edu.teu.ac.jp>
2023-06-13 15:22:32 +0900

    問題5

--- pygame_image.py
+++ pygame_image.py
@@ -17,6 +17,10 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        if tmr % 2 == 0:
+            screen.blit(koukaton_list[0], [300, 200])
+        else:
+            screen.blit(koukaton_list[1], [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
